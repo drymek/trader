029fbb157ba20092376f1175f78fd23fcff87fbf
Marcin Dryka <marcin@brainhub.pl>
2022-04-09 11:57:16 +0200

    First version of CRUD test

--- tests/e2e/crud_test.go
+++ tests/e2e/crud_test.go
@@ -5,107 +5,15 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"testing"
 
-	"dryka.pl/trader/internal/application/config"
 	"dryka.pl/trader/internal/application/server"
-	"dryka.pl/trader/internal/domain/user/model"
-	repository2 "dryka.pl/trader/internal/domain/user/repository"
-	"dryka.pl/trader/internal/domain/user/service"
-	"dryka.pl/trader/internal/infrastructure/persistence/inmemory/repository"
 	testCmpopts "dryka.pl/trader/tests/cmpopts"
-	"dryka.pl/trader/tests/mock"
-	"github.com/caarlos0/env/v6"
 	"github.com/google/go-cmp/cmp"
-	"github.com/stretchr/testify/suite"
 )
 
-type CrudSuite struct {
-	suite.Suite
-	AppDependencies server.Dependencies
-}
-
-func TestCrudSuite(t *testing.T) {
-	s := new(CrudSuite)
-	opts := env.Options{Environment: map[string]string{
-		"DATABASE_FILE": "database/sqlite/database_test.sqlite",
-	}}
-	c, err := config.NewConfig(opts)
-	if err != nil {
-		t.Fatal("invalid config")
-	}
-
-	accountRepository := repository.NewAccountRepository()
-	s.AppDependencies = server.Dependencies{
-		Logger:            mock.NewNullLogger(),
-		Config:            c,
-		CrudService:       service.NewAccountService(accountRepository),
-		AccountRepository: accountRepository,
-	}
-
-	suite.Run(t, s)
-}
-
-func (s *CrudSuite) SetupTest() {
-	account, err := s.AppDependencies.AccountRepository.Find("123")
-	if err != nil {
-		return
-	}
-	err = s.AppDependencies.AccountRepository.Delete(account.(*model.Account).ID)
-	s.Nil(err)
-}
-
-func (s *CrudSuite) TestCreate() {
+func (s *Suite) TestCreate() {
 	srv := httptest.NewServer(server.NewServer(s.AppDependencies))
 	defer srv.Close()
-
-	accountJSON := `{
-		"id": "123",
-		"owner": "Marcin Dryka",
-		"balance": "100.0",
-		"currency": "PLN",
-		"account_number": 123456789
-	}`
-
-	requestBody := []byte(accountJSON)
-	res, err := http.Post(srv.URL+"/accounts", "application/json", bytes.NewBuffer(requestBody))
-	s.Nil(err)
-
-	got, err := ioutil.ReadAll(res.Body)
-	err2 := res.Body.Close()
-	s.Nil(err)
-	s.Nil(err2)
-
-	s.Equal([]byte("{\"id\":\"123\"}\n"), got)
-	s.Equal(http.StatusCreated, res.StatusCode, "Expected status code 201")
-
-	want := model.Account{
-		ID:            "123",
-		Owner:         "Marcin Dryka",
-		Balance:       "100.0",
-		Currency:      "PLN",
-		AccountNumber: 123456789,
-	}
-	existing, err := s.AppDependencies.AccountRepository.Find("123")
-	s.Nil(err)
-
-	if diff := cmp.Diff(want, *existing.(*model.Account)); diff != "" {
-		s.Failf("value mismatch", "(-want +got):\n%v", diff)
-	}
-}
-
-func (s *CrudSuite) TestCrateDupplicate() {
-	srv := httptest.NewServer(server.NewServer(s.AppDependencies))
-	defer srv.Close()
-
-	err := s.AppDependencies.AccountRepository.Create(&model.Account{
-		ID:            "123",
-		Owner:         "Marcin Dryka",
-		Balance:       "100.0",
-		Currency:      "PLN",
-		AccountNumber: 123456789,
-	})
-	s.NoError(err)
 
 	accountJSON := `{
 		"id": "123",
@@ -116,7 +24,7 @@ func (s *CrudSuite) TestCrateDupplicate() {
 	}`
 
 	requestBody := []byte(accountJSON)
-	res, err := http.Post(srv.URL+"/accounts", "application/json", bytes.NewBuffer(requestBody))
+	res, err := http.Post(srv.URL+"/account", "application/json", bytes.NewBuffer(requestBody))
 	s.Nil(err)
 
 	got, err := ioutil.ReadAll(res.Body)
@@ -124,23 +32,56 @@ func (s *CrudSuite) TestCrateDupplicate() {
 	s.Nil(err)
 	s.Nil(err2)
 
-	s.Equal([]byte(`{"error":"Bad Request"}`), got)
+	s.Equal([]byte(`{"id":"123"}\n`), got)
+	s.Equal(http.StatusCreated, res.StatusCode, "Expected status code 201")
+}
+
+func (s *Suite) TestCrateDupplicate() {
+	srv := httptest.NewServer(server.NewServer(s.AppDependencies))
+	defer srv.Close()
+
+	s.AppDependencies.AccountRepository.Create(user.Account{
+		ID:            "123",
+		Owner:         "Marcin Dryka",
+		Balance:       100.0,
+		Currency:      "PLN",
+		AccountNumber: 123456789,
+	})
+
+	accountJSON := `{
+		"id": "123",
+		"owner": "Marcin Dryka",
+		"balance": "100.0",
+		"currency": "PLN",
+		"account_number": 123456789,
+	}`
+
+	requestBody := []byte(accountJSON)
+	res, err := http.Post(srv.URL+"/account", "application/json", bytes.NewBuffer(requestBody))
+	s.Nil(err)
+
+	got, err := ioutil.ReadAll(res.Body)
+	err2 := res.Body.Close()
+	s.Nil(err)
+	s.Nil(err2)
+
+	s.Equal([]byte(`{"error":"Bad request"}`), got)
 	s.Equal(http.StatusBadRequest, res.StatusCode, "Expected status code 400")
 }
 
-func (s *CrudSuite) TestFetch() {
+func (s *Suite) TestFetch() {
 	srv := httptest.NewServer(server.NewServer(s.AppDependencies))
 	defer srv.Close()
-	account := &model.Account{
+	account := user.Account{
 		ID:            "123",
 		Owner:         "Marcin Dryka",
-		Balance:       "100.0",
+		Balance:       100.0,
 		Currency:      "PLN",
 		AccountNumber: 123456789,
 	}
-	err := s.AppDependencies.AccountRepository.Create(account)
-	s.Nil(err)
-	res, err := http.Get(srv.URL + "/accounts/" + account.ID)
+	s.AppDependencies.AccountRepository.Create(account)
+
+	res, err := http.Get(srv.URL + "/account/" + account.ID)
 	s.Nil(err)
 
 	got, err := ioutil.ReadAll(res.Body)
@@ -149,26 +90,19 @@ func (s *CrudSuite) TestFetch() {
 	s.Nil(err2)
 
 	transformJSON := testCmpopts.TransformJSON()
-	want := `{
-		"id": "123",
-		"owner": "Marcin Dryka",
-		"balance": "100.0",
-		"currency": "PLN",
-		"account_number": 123456789
-	}`
-	if diff := cmp.Diff([]byte(want), got, cmp.Options{transformJSON}); diff != "" {
+	want := account
+	if diff := cmp.Diff(want, got, cmp.Options{transformJSON}); diff != "" {
 		s.Failf("value mismatch", "(-want +got):\n%v", diff)
 	}
 
 	s.Equal(http.StatusOK, res.StatusCode, "Expected status code 400")
-
 }
 
-func (s *CrudSuite) TestFetchNotFound() {
+func (s *Suite) TestFetchNotFound() {
 	srv := httptest.NewServer(server.NewServer(s.AppDependencies))
 	defer srv.Close()
 
-	res, err := http.Get(srv.URL + "/accounts/fake-id")
+	res, err := http.Get(srv.URL + "/account/fake-id")
 	s.Nil(err)
 
 	got, err := ioutil.ReadAll(res.Body)
@@ -176,11 +110,10 @@ func (s *CrudSuite) TestFetchNotFound() {
 	s.Nil(err)
 	s.Nil(err2)
 
-	s.Equal([]byte(`{"error":"Not Found"}`), got)
 	s.Equal(http.StatusNotFound, res.StatusCode, "Expected status code 400")
 }
 
-func (s *CrudSuite) TestUpdateNewObject() {
+func (s *Suite) TestUpdateNewObject() {
 	srv := httptest.NewServer(server.NewServer(s.AppDependencies))
 	defer srv.Close()
 
@@ -189,14 +122,11 @@ func (s *CrudSuite) TestUpdateNewObject() {
 		"owner": "Marcin Dryka",
 		"balance": "100.0",
 		"currency": "PLN",
-		"account_number": 123456789
+		"account_number": 123456789,
 	}`
 
 	requestBody := []byte(accountJSON)
-	req, err := http.NewRequest(http.MethodPut, srv.URL+"/accounts", bytes.NewBuffer(requestBody))
-	req.Header.Set("Content-Type", "application/json")
-	s.Nil(err)
-	res, err := http.DefaultClient.Do(req)
+	res, err := http.Put(srv.URL+"/account", "application/json", bytes.NewBuffer(requestBody))
 	s.Nil(err)
 
 	got, err := ioutil.ReadAll(res.Body)
@@ -204,37 +134,32 @@ func (s *CrudSuite) TestUpdateNewObject() {
 	s.Nil(err)
 	s.Nil(err2)
 
-	s.Equal([]byte("{\"id\":\"123\"}\n"), got)
+	s.Equal([]byte(`{"id":"123"}\n`), got)
 	s.Equal(http.StatusCreated, res.StatusCode, "Expected status code 201")
 }
 
-func (s *CrudSuite) TestUpdate() {
+func (s *Suite) TestUpdate() {
 	srv := httptest.NewServer(server.NewServer(s.AppDependencies))
 	defer srv.Close()
 
-	account := &model.Account{
+	s.AppDependencies.AccountRepository.Create(user.Account{
 		ID:            "123",
 		Owner:         "Marcin Dryka",
-		Balance:       "100.0",
+		Balance:       100.0,
 		Currency:      "PLN",
 		AccountNumber: 123456789,
-	}
-	err := s.AppDependencies.AccountRepository.Create(account)
-	s.NoError(err)
+	})
 
 	accountJSON := `{
 		"id": "123",
 		"owner": "Marcin Dryka",
 		"balance": "100.0",
 		"currency": "PLN",
-		"account_number": 123456789
+		"account_number": 123456789,
 	}`
 
 	requestBody := []byte(accountJSON)
-	req, err := http.NewRequest(http.MethodPut, srv.URL+"/accounts", bytes.NewBuffer(requestBody))
-	req.Header.Set("Content-Type", "application/json")
-	s.Nil(err)
-	res, err := http.DefaultClient.Do(req)
+	res, err := http.Put(srv.URL+"/account", "application/json", bytes.NewBuffer(requestBody))
 	s.Nil(err)
 
 	got, err := ioutil.ReadAll(res.Body)
@@ -242,29 +167,30 @@ func (s *CrudSuite) TestUpdate() {
 	s.Nil(err)
 	s.Nil(err2)
 
-	s.Equal([]byte("{\"id\":\"123\"}\n"), got)
+	s.Equal([]byte(`{"id":"123"}\n`), got)
 	s.Equal(http.StatusOK, res.StatusCode, "Expected status code 200")
 }
 
-func (s *CrudSuite) TestRemove() {
+func (s *Suite) TestPartialUpdate() {
 	srv := httptest.NewServer(server.NewServer(s.AppDependencies))
 	defer srv.Close()
 
-	account := &model.Account{
+	account := user.Account{
 		ID:            "123",
 		Owner:         "Marcin Dryka",
-		Balance:       "100.0",
+		Balance:       100.0,
 		Currency:      "PLN",
 		AccountNumber: 123456789,
 	}
+	s.AppDependencies.AccountRepository.Create(account)
 
-	err := s.AppDependencies.AccountRepository.Create(account)
-	s.Nil(err)
+	accountJSON := `{
+		"balance": "200.0",
+		"currency": "USD",
+	}`
 
-	req, err := http.NewRequest(http.MethodDelete, srv.URL+"/accounts/"+account.ID, bytes.NewBuffer([]byte("")))
-	req.Header.Set("Content-Type", "application/json")
-	s.Nil(err)
-	res, err := http.DefaultClient.Do(req)
+	requestBody := []byte(accountJSON)
+	res, err := http.Patch(srv.URL+"/account/"+account.ID, "application/json", bytes.NewBuffer(requestBody))
 	s.Nil(err)
 
 	got, err := ioutil.ReadAll(res.Body)
@@ -272,23 +198,41 @@ func (s *CrudSuite) TestRemove() {
 	s.Nil(err)
 	s.Nil(err2)
 
-	s.Equal([]byte(""), got)
+	s.Equal([]byte(`{"id":"123"}\n`), got)
+
+	want := user.Account{
+		ID:            "123",
+		Owner:         "Marcin Dryka",
+		Balance:       200.0,
+		Currency:      "USD",
+		AccountNumber: 123456789,
+	}
+	existing := s.AppDependencies.AccountRepository.Find("123")
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		s.Failf("value mismatch", "(-want +got):\n%v", diff)
+	}
 
 	s.Equal(http.StatusOK, res.StatusCode, "Expected status code 200")
-
-	_, err = s.AppDependencies.AccountRepository.Find("123")
-	s.ErrorIs(err, repository2.ErrAccountNotFound)
 }
 
-func (s *CrudSuite) TestRemoveFakeId() {
+func (s *Suite) TestRemove() {
 	srv := httptest.NewServer(server.NewServer(s.AppDependencies))
 	defer srv.Close()
 
-	req, err := http.NewRequest(http.MethodDelete, srv.URL+"/accounts/fake-id", bytes.NewBuffer([]byte("")))
-	req.Header.Set("Content-Type", "application/json")
-	s.Nil(err)
-	res, err := http.DefaultClient.Do(req)
+	s.AppDependencies.AccountRepository.Create(user.Account{
+		ID:            "123",
+		Owner:         "Marcin Dryka",
+		Balance:       100.0,
+		Currency:      "PLN",
+		AccountNumber: 123456789,
+	})
+
+	res, err := http.Delete(srv.URL+"/account/"+account.ID, "application/json", bytes.NewBuffer(requestBody))
+	s.Equal(http.StatusOK, res.StatusCode, "Expected status code 200")
+
 	s.Nil(err)
 
-	s.Equal(http.StatusNotFound, res.StatusCode, "Expected status code 404")
+	_, err = s.AppDependencies.AccountRepository.Find("123")
+	s.ErrorIs(err, user.ErrAccountNotFound)
 }
