71197505d87cb440dbd2380ac58e97896853083e
Marcin Dryka <marcin@brainhub.pl>
2022-04-10 23:01:15 +0200

    Mongo support

--- go.mod
+++ go.mod
@@ -7,21 +7,32 @@ require (
 	github.com/go-kit/kit v0.12.0
 	github.com/go-kit/log v0.2.0
 	github.com/google/go-cmp v0.5.7
+	github.com/google/uuid v1.3.0
 	github.com/gorilla/mux v1.8.0
+	github.com/mattn/go-sqlite3 v1.14.12
 	github.com/newrelic/go-agent/v3 v3.15.2
 	github.com/shopspring/decimal v1.3.1
 	github.com/stretchr/testify v1.7.1
+	go.mongodb.org/mongo-driver v1.9.0
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.1.0 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/crypto v0.0.0-20210915214749-c084706c2272 // indirect
 	golang.org/x/net v0.0.0-20210917221730-978cfadd31cf // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
--- internal/infrastructure/persistence/test/repository/account_test.go
+++ internal/infrastructure/persistence/test/repository/account_test.go
@@ -1,21 +1,29 @@
 package repository
 
 import (
+	"context"
+	"sync"
 	"testing"
+	"time"
 
+	"dryka.pl/trader/internal/application/config"
 	"dryka.pl/trader/internal/domain/user/model"
 	"dryka.pl/trader/internal/domain/user/repository"
 	inmemory "dryka.pl/trader/internal/infrastructure/persistence/inmemory/repository"
+	mongox "dryka.pl/trader/internal/infrastructure/persistence/mongo/repository"
 	sqlitex "dryka.pl/trader/internal/infrastructure/persistence/sqlite"
 	sqlite "dryka.pl/trader/internal/infrastructure/persistence/sqlite/repository"
 	"dryka.pl/trader/tests/database"
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type RepositoryIntegrationSuite struct {
 	suite.Suite
 	repositories map[string]repository.AccountRepository
+	mu           sync.Mutex
 }
 
 func TestServiceSuite(t *testing.T) {
@@ -27,31 +35,51 @@ func TestServiceSuite(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	connection, err := sqlitex.NewConnection("../../../../../database/sqlite/database_test.sqlite")
+	connection, _ := sqlitex.NewConnection("../../../../../database/sqlite/database_test.sqlite")
+	c, err := config.NewConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+	client, err := mongo.NewClient(options.Client().ApplyURI(c.GetMongoURI()))
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
 	s.repositories = make(map[string]repository.AccountRepository)
+	s.repositories["mongo"] = mongox.NewAccountRepository(client)
 	s.repositories["inmemory"] = inmemory.NewAccountRepository()
 	s.repositories["sqlite"] = sqlite.NewAccountRepository(connection)
+	_ = mongox.NewAccountRepository(client)
+	_ = inmemory.NewAccountRepository()
+	_ = sqlite.NewAccountRepository(connection)
+	s.mu = sync.Mutex{}
 	suite.Run(t, s)
 }
 
 func (s *RepositoryIntegrationSuite) SetupTest() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	for i := range s.repositories {
 		account, err := s.repositories[i].Find("123")
 		if err != nil {
 			return
 		}
-		_ = s.repositories[i].Delete(account.(*model.Account).ID)
+		err = s.repositories[i].Delete(account.(*model.Account).ID)
+		s.NoError(err)
 	}
 }
 
 func (s *RepositoryIntegrationSuite) TestCreateAndFetch() {
 	for i := range s.repositories {
 		s.T().Run(i, func(t *testing.T) {
+			s.mu.Lock()
+			defer s.mu.Unlock()
 			account := &model.Account{
 				ID:            "123",
 				Owner:         "Marcin Dryka",
@@ -72,6 +100,8 @@ func (s *RepositoryIntegrationSuite) TestCreateAndFetch() {
 func (s *RepositoryIntegrationSuite) TestDelete() {
 	for i := range s.repositories {
 		s.T().Run(i, func(t *testing.T) {
+			s.mu.Lock()
+			defer s.mu.Unlock()
 			account := &model.Account{
 				ID:            "123",
 				Owner:         "Marcin Dryka",
@@ -96,6 +126,8 @@ func (s *RepositoryIntegrationSuite) TestDelete() {
 func (s *RepositoryIntegrationSuite) TestUpdate() {
 	for i := range s.repositories {
 		s.T().Run(i, func(t *testing.T) {
+			s.mu.Lock()
+			defer s.mu.Unlock()
 			account := &model.Account{
 				ID:            "123",
 				Owner:         "Marcin Dryka",
